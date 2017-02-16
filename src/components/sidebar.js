f94c4010cfb121279c7b01cab25ef1e54170c439
rusticman <russellwheatley85@gmail.com>
2017-02-16 11:21:20 +0000

    changed a tags

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -29,15 +29,36 @@ window.onscroll= menuScroll;
 
   return(
 
-    <nav className="menu"><a href="#work-section" className={"about menu-item " + aboutSidebar}>
-        <div className="menu-link"><span className={"title " + aboutColour}>about</span>
-      </div></a><a href="#portfolioContainer" className={"portfolio menu-item " + portfolioSidebar}>
-        <div className="menu-link"><span className={"title " + portfolioColour}>portfolio</span>
-      </div></a><a href="#photographyContainer" className={"contact menu-item " + photographySidebar}>
-        <div className="menu-link"><span className={"title " + photographyColour}>photography</span>
-      </div></a><a href="#contactContainer" className="contact menu-item">
-        <div className="menu-link"><span className={"title " + contactColour}>contact me</span>
-                        </div></a></nav>
+      <nav className="menu">
+        <div className={"menu-link " + aboutSidebar}>
+          <div className="buffer">
+            <a href="#aboutMeContainer" className="about menu-item" >
+              <span className={"title " + aboutColour}>about</span>
+            </a>
+          </div>
+        </div>
+        <div className={"menu-link " + portfolioSidebar}>
+          <div className="buffer">
+            <a href="#portfolioContainer" className="portfolio menu-item">
+              <span className={"title " + portfolioColour}>portfolio</span>
+            </a>
+          </div>
+        </div>
+        <div className={"menu-link " + photographySidebar}>
+          <div className="buffer">
+            <a href="#photographyContainer" className="contact menu-item">
+              <span className={"title " + photographyColour}>photography</span>
+            </a>
+          </div>
+        </div>
+        <div className="menu-link">
+          <div className="buffer">
+            <a href="#contactContainer" className="contact menu-item">
+              <span className={"title " + contactColour}>contact me</span>
+            </a>
+          </div>
+        </div>
+      </nav>
   )
 }
 
